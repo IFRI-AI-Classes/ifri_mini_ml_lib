21a705346f291567480235a3da816a9ae07bd517
Shado-Jony <jiovanyadonon8@gmail.com>
2025-04-17 21:02:15 +0100

    Update inertia.py
    Mise à jour des commentaires de inertia.py

--- Unsupervised/clustering/clustering/metrics/inertia.py
+++ Unsupervised/clustering/clustering/metrics/inertia.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def calculate_inertia(data, labels, centroids):
     """
-    Calcule l'inertie d'un modèle de clustering.
-    
-    :param data: Données d'entrée (numpy array)
-    :param labels: Labels de cluster pour chaque point (numpy array)
-    :param centroids: Centres des clusters (numpy array)
-    :return: Inertie totale
+    Computes the inertia of a clustering model.
+
+    :param data: Input data (numpy array)
+    :param labels: Cluster labels for each point (numpy array)
+    :param centroids: Cluster centers (numpy array)
+    :Return: Total inertia
     """
     inertia = 0
     for i in range(len(data)):
