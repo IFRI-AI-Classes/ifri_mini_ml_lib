205cbb364f5cd5a2a6e65d0726e18602d65b02f6
Shado-Jony <jiovanyadonon8@gmail.com>
2025-04-23 01:25:21 +0100

    Update utils.py

--- Unsupervised/clustering/clustering/utils/utils.py
+++ Unsupervised/clustering/clustering/utils/utils.py
@@ -2,13 +2,32 @@ import numpy as np
 
 def euclidean_distance(x1, x2):
     """
-    Computes the Euclidean distance between two vectors.
+    Description:
+    ------------
+    Computes the Euclidean distance between two vectors. Euclidean distance is the straight-line
+    distance between two points in Euclidean space, calculated as the square root of the sum of
+    the squared differences between corresponding elements of the vectors.
 
-    Parameters:
+    Arguments:
+    -----------
     - x1 (numpy.ndarray): First vector.
     - x2 (numpy.ndarray): Second vector.
 
+    Functions:
+    -----------
+    - Calculates the element-wise difference between the two vectors.
+    - Squares each element of the resulting vector.
+    - Sums the squared elements.
+    - Takes the square root of the sum.
+
     Returns:
+    --------
     - float: The Euclidean distance between x1 and x2.
+
+    Example:
+    ---------
+    distance = euclidean_distance(np.array([1, 2]), np.array([4, 6]))
+    print(f"Euclidean distance: {distance}")
+    """t: The Euclidean distance between x1 and x2.
     """
     return np.sqrt(np.sum((x1 - x2)**2))
