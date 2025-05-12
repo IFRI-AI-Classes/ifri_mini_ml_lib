3fb93acccd903fd22a0e1937efd3350040aec46e
Folfed <143909304+Folfed@users.noreply.github.com>
2025-05-12 17:30:07 +0100

    Update __init__.py

--- ifri_mini_ml_lib/neural_networks/__init__.py
+++ ifri_mini_ml_lib/neural_networks/__init__.py
@@ -1,5 +1,6 @@
-from .mlp import MLP
+from .mlp_classifier import MLPClassifier as mlp_classifier
+from .mlp_regressor import MLPRegressor as mlp_regressor
 
 __all__ =[
-    "MLP"
+    "mlp_classifier", "mlp_regressor",
 ]
