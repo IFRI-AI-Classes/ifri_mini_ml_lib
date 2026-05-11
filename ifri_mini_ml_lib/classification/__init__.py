c9c52623a147d9b4a9b171c8c423ad2ef903621c
femi-cloud <femi13sanya@gmail.com>
2026-05-12 00:05:02 +0100

    docs: add interactive notebooks for Random Forest Regressor and Naive Bayes

--- ifri_mini_ml_lib/classification/__init__.py
+++ ifri_mini_ml_lib/classification/__init__.py
@@ -1,9 +1,12 @@
 from .decision_tree import DecisionTree
 from .knn import KNN
 from .logistic_regression import LogisticRegression
+from .naive_bayes import NaiveBayes
+
 
 __all__ = [
     "DecisionTree",
     "KNN",
-    "LogisticRegression"
+    "LogisticRegression",
+    "NaiveBayes"
 ]
--- ifri_mini_ml_lib/regression/__init__.py
+++ ifri_mini_ml_lib/regression/__init__.py
@@ -1,9 +1,10 @@
 from .linear_regression import LinearRegression
 from .polynomial_regression import PolynomialRegression
 from .svr import SVR
-
+from .random_forest_regression import RandomForestRegressor
 __all__ = [
     "LinearRegression",
     "PolynomialRegression",
-    "SVR"
+    "SVR",
+    "RandomForestRegressor"
 ]
