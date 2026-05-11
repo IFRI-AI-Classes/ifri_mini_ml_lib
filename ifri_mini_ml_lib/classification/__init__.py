b013a9f9b0e0c69b4b80571e6c47cc350b469587
ulkbm2k25 <bamigbolaulrich@gmail.com>
2026-05-12 00:13:52 +0100

    Ajout de Random Forest pour la classification et testsunitaires

--- ifri_mini_ml_lib/classification/__init__.py
+++ ifri_mini_ml_lib/classification/__init__.py
@@ -1,9 +1,11 @@
 from .decision_tree import DecisionTree
 from .knn import KNN
 from .logistic_regression import LogisticRegression
+from .random_forest import RandomForest
 
 __all__ = [
     "DecisionTree",
     "KNN",
-    "LogisticRegression"
+    "LogisticRegression",
+    "RandomForest"
 ]
