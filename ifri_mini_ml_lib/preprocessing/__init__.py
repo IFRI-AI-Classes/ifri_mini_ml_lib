c2aa459351dc86516cb7e428a080585bc664e361
FreudArthur <bokossafreud6@gmail.com>
2026-05-14 11:02:52 +0100

    feat: clean up empty code change sections in the changes log Adding notebook for multilabel film classification Update __init__.py

---
+++ ifri_mini_ml_lib/preprocessing/__init__.py
@@ -0,0 +1,9 @@
+from . import preparation
+from . import text
+from . import dimensionality_reduction
+
+__all__ = [
+    "preparation",
+    "text",
+    "dimensionality_reduction"
+]
---
+++ ifri_mini_ml_lib/preprocessing/text/__init__.py
@@ -0,0 +1,7 @@
+from . import stop_word
+from . import test_stopwords
+
+__all__ = [
+    "stop_word",
+    "test_stopwords"
+]
