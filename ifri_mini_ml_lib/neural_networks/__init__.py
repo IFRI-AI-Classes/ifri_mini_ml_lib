80ad7c84f61a3c648e6be09917cf855b5cd213e9
7jwares <jauresokpeicha7@gmail.com>
2026-05-14 04:30:17 +0000

    Comparative benchmark of Perceptron classes of ifri_mini_ml_lib and sklearn > Main content : - Information about the Perceptron: Definition, purpose, history, algorithm, importance, limitations, ... - Implementations :   - Preparation of three different types of datasets to demonstrate characteristics of the `Perceptron` (purpose, appropriate datasets, limitations)   - Sequencing of processes (`importing`, `splitting`, `instantiation`, `fitting`, `prediction`, then `evaluation`) with `ifri_mini_ml_lib` and `sklearn` on these three data - Summary tables of performance (`evaluation metrics`, `fitting runtime`, `prediction runtime`) - ...

--- ifri_mini_ml_lib/neural_networks/__init__.py
+++ ifri_mini_ml_lib/neural_networks/__init__.py
@@ -1,6 +1,9 @@
 from .mlp_classifier import MLPClassifier
 from .mlp_regressor import MLPRegressor
 
+from .perceptron import Perceptron
+
 __all__ =[
     "MLPClassifier", "MLPRegressor",
+    "Perceptron",
 ]
