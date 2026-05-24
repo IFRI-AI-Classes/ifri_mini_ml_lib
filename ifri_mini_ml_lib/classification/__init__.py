4fce50eef8194886b67d4cbb06732a022fe5d80c
Rosas Behoundja <perrierosas@gmail.com>
2026-05-24 09:53:13 +0100

    chore: update CI configuration to include 'dev' branch and enhance notebook structure

--- ifri_mini_ml_lib/classification/__init__.py
+++ ifri_mini_ml_lib/classification/__init__.py
@@ -2,9 +2,12 @@ from .decision_tree import DecisionTree
 from .knn import KNN
 from .logistic_regression import LogisticRegression
 from .random_forest import RandomForest
+from .naive_bayes import NaiveBayes
+
 __all__ = [
     "DecisionTree",
     "KNN",
     "LogisticRegression",
-    "RandomForest"
+    "RandomForest",
+    "NaiveBayes",
 ]
